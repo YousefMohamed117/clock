c8350161aa6cf7596ca7378b0aa62c29fea5ef45
YousefMohamed117 <114279536+YousefMohamed117@users.noreply.github.com>
2023-10-23 11:46:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let sec = document.querySelector(".sec");
+let min = document.querySelector(".min");
+let hour = document.querySelector(".hour");
+
+setInterval(() => {
+  let seconds = new Date().getSeconds();
+  let minutes = new Date().getMinutes();
+  let hours = new Date().getHours();
+
+    console.log(seconds , minutes , hours);
+
+  sec.style.transform = `rotate(${seconds * 6}deg)  ` 
+  min.style.transform = `rotate(${(minutes * 6) + (seconds/10)}deg)  ` 
+  hour.style.transform = `rotate(${(hours * 30)+(minutes/2)}deg)  ` 
+},1000);
